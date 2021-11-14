3d4b089677fea35cf06ca0156b101c62c7ec135a
bhanubenak <bhanubenak@gmail.com>
2021-11-14 21:58:46 +0530

    second commit

--- Airline Management System Project/home.Designer.vb
+++ Airline Management System Project/home.Designer.vb
@@ -23,17 +23,15 @@ Partial Class home
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button4 = New System.Windows.Forms.Button()
         Me.Button3 = New System.Windows.Forms.Button()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.btn_flight_update = New System.Windows.Forms.Button()
+        Me.btn_flight_info = New System.Windows.Forms.Button()
         Me.Panel11 = New System.Windows.Forms.Panel()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.btnminimize = New System.Windows.Forms.Button()
         Me.btnmaximize = New System.Windows.Forms.Button()
         Me.btnclose = New System.Windows.Forms.Button()
-        Me.Button4 = New System.Windows.Forms.Button()
-        Me.Button5 = New System.Windows.Forms.Button()
-        Me.Button6 = New System.Windows.Forms.Button()
         Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
@@ -41,11 +39,9 @@ Partial Class home
         '
         Me.Panel1.BackColor = System.Drawing.SystemColors.WindowFrame
         Me.Panel1.Controls.Add(Me.Button4)
-        Me.Panel1.Controls.Add(Me.Button5)
-        Me.Panel1.Controls.Add(Me.Button6)
         Me.Panel1.Controls.Add(Me.Button3)
-        Me.Panel1.Controls.Add(Me.Button2)
-        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.btn_flight_update)
+        Me.Panel1.Controls.Add(Me.btn_flight_info)
         Me.Panel1.Controls.Add(Me.Panel11)
         Me.Panel1.Controls.Add(Me.Label1)
         Me.Panel1.ForeColor = System.Drawing.SystemColors.AppWorkspace
@@ -54,50 +50,65 @@ Partial Class home
         Me.Panel1.Size = New System.Drawing.Size(1340, 720)
         Me.Panel1.TabIndex = 0
         '
+        'Button4
+        '
+        Me.Button4.FlatAppearance.BorderColor = System.Drawing.Color.Silver
+        Me.Button4.FlatAppearance.BorderSize = 2
+        Me.Button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkSalmon
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.ForeColor = System.Drawing.Color.White
+        Me.Button4.Location = New System.Drawing.Point(27, 265)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(225, 34)
+        Me.Button4.TabIndex = 49
+        Me.Button4.Text = "Button4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
         'Button3
         '
         Me.Button3.FlatAppearance.BorderColor = System.Drawing.Color.Silver
         Me.Button3.FlatAppearance.BorderSize = 2
-        Me.Button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkSalmon
         Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Button3.ForeColor = System.Drawing.Color.White
-        Me.Button3.Location = New System.Drawing.Point(17, 268)
+        Me.Button3.Location = New System.Drawing.Point(27, 210)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(225, 34)
         Me.Button3.TabIndex = 46
         Me.Button3.Text = "Button3"
         Me.Button3.UseVisualStyleBackColor = True
         '
-        'Button2
+        'btn_flight_update
         '
-        Me.Button2.FlatAppearance.BorderColor = System.Drawing.Color.Silver
-        Me.Button2.FlatAppearance.BorderSize = 2
-        Me.Button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button2.ForeColor = System.Drawing.Color.White
-        Me.Button2.Location = New System.Drawing.Point(17, 213)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(225, 34)
-        Me.Button2.TabIndex = 45
-        Me.Button2.Text = "Button2"
-        Me.Button2.UseVisualStyleBackColor = True
+        Me.btn_flight_update.FlatAppearance.BorderColor = System.Drawing.Color.Silver
+        Me.btn_flight_update.FlatAppearance.BorderSize = 2
+        Me.btn_flight_update.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkSalmon
+        Me.btn_flight_update.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_flight_update.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_flight_update.ForeColor = System.Drawing.Color.White
+        Me.btn_flight_update.Location = New System.Drawing.Point(27, 155)
+        Me.btn_flight_update.Name = "btn_flight_update"
+        Me.btn_flight_update.Size = New System.Drawing.Size(225, 34)
+        Me.btn_flight_update.TabIndex = 45
+        Me.btn_flight_update.Text = "FLight Updates"
+        Me.btn_flight_update.UseVisualStyleBackColor = True
         '
-        'Button1
+        'btn_flight_info
         '
-        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.Silver
-        Me.Button1.FlatAppearance.BorderSize = 2
-        Me.Button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.ForeColor = System.Drawing.Color.White
-        Me.Button1.Location = New System.Drawing.Point(17, 161)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(225, 34)
-        Me.Button1.TabIndex = 44
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btn_flight_info.FlatAppearance.BorderColor = System.Drawing.Color.Silver
+        Me.btn_flight_info.FlatAppearance.BorderSize = 2
+        Me.btn_flight_info.FlatAppearance.MouseOverBackColor = System.Drawing.Color.DarkSalmon
+        Me.btn_flight_info.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn_flight_info.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn_flight_info.ForeColor = System.Drawing.Color.White
+        Me.btn_flight_info.Location = New System.Drawing.Point(27, 103)
+        Me.btn_flight_info.Name = "btn_flight_info"
+        Me.btn_flight_info.Size = New System.Drawing.Size(225, 34)
+        Me.btn_flight_info.TabIndex = 44
+        Me.btn_flight_info.Text = "Flight info"
+        Me.btn_flight_info.UseVisualStyleBackColor = True
         '
         'Panel11
         '
@@ -158,51 +169,6 @@ Partial Class home
         Me.btnclose.TabIndex = 13
         Me.btnclose.UseVisualStyleBackColor = False
         '
-        'Button4
-        '
-        Me.Button4.FlatAppearance.BorderColor = System.Drawing.Color.Silver
-        Me.Button4.FlatAppearance.BorderSize = 2
-        Me.Button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button4.ForeColor = System.Drawing.Color.White
-        Me.Button4.Location = New System.Drawing.Point(17, 323)
-        Me.Button4.Name = "Button4"
-        Me.Button4.Size = New System.Drawing.Size(225, 34)
-        Me.Button4.TabIndex = 49
-        Me.Button4.Text = "Button4"
-        Me.Button4.UseVisualStyleBackColor = True
-        '
-        'Button5
-        '
-        Me.Button5.FlatAppearance.BorderColor = System.Drawing.Color.Silver
-        Me.Button5.FlatAppearance.BorderSize = 2
-        Me.Button5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button5.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button5.ForeColor = System.Drawing.Color.White
-        Me.Button5.Location = New System.Drawing.Point(17, 378)
-        Me.Button5.Name = "Button5"
-        Me.Button5.Size = New System.Drawing.Size(225, 34)
-        Me.Button5.TabIndex = 48
-        Me.Button5.Text = "Button5"
-        Me.Button5.UseVisualStyleBackColor = True
-        '
-        'Button6
-        '
-        Me.Button6.FlatAppearance.BorderColor = System.Drawing.Color.Silver
-        Me.Button6.FlatAppearance.BorderSize = 2
-        Me.Button6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer))
-        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button6.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button6.ForeColor = System.Drawing.Color.White
-        Me.Button6.Location = New System.Drawing.Point(17, 432)
-        Me.Button6.Name = "Button6"
-        Me.Button6.Size = New System.Drawing.Size(225, 34)
-        Me.Button6.TabIndex = 47
-        Me.Button6.Text = "Button6"
-        Me.Button6.UseVisualStyleBackColor = True
-        '
         'home
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
@@ -229,9 +195,7 @@ Partial Class home
     Friend WithEvents Label1 As Label
     Friend WithEvents Panel11 As Panel
     Friend WithEvents Button3 As Button
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btn_flight_update As Button
+    Friend WithEvents btn_flight_info As Button
     Friend WithEvents Button4 As Button
-    Friend WithEvents Button5 As Button
-    Friend WithEvents Button6 As Button
 End Class
