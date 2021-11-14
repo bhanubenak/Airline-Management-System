2362c4c6a0755bd73e41006a4d0dc00f78ffc64d
bhanubenak <bhanubenak@gmail.com>
2021-11-14 22:08:37 +0530

    home user pg created

--- Airline Management System Project/home_user.vb
+++ Airline Management System Project/home_user.vb
@@ -1,3 +1,15 @@
 ﻿Public Class home_user
+    Private Sub btnclose_home_user_Click(sender As Object, e As EventArgs) Handles btnclose_home_user.Click
+        Close()
+    End Sub
+
+    Private Sub btnmaximize_home_user_Click(sender As Object, e As EventArgs) Handles btnmaximize_home_user.Click
+        WindowState = FormWindowState.Maximized
+    End Sub
+
+    Private Sub btnminimize_home_user_Click(sender As Object, e As EventArgs) Handles btnminimize_home_user.Click
+        WindowState = FormWindowState.Minimized
+    End Sub
+
 
 End Class
